96f0c09b97d9f5b05d91225ec57b0cd04e069b21
Roshan <roshanchongder9@gmail.com>
2023-01-29 20:45:39 +0530

    Initial changes done.

---
+++ src/app/common-utils/common-util.ts
@@ -0,0 +1,13 @@
+export enum TOKEN {
+    key = 'USER_TOKEN'
+}
+
+export enum PASSWORD_SRENGTH {
+    WEAK = 'Weak',
+    MEDIUM = 'Medium',
+    STRONG = 'Strong'
+}
+
+export const EMAIL_REGEX = /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/;
+export const STRONG_PASSWORD_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/;
+export const MEDIUM_PASSWORD_REGEX = /((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))/;
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserSignupComponent } from './user-signup/user-signup.component';
 import { UserRoutingModule } from './user-routing.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,12 @@ import { UserRoutingModule } from './user-routing.module';
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    UserService
   ]
 })
 export class UserModule { }
